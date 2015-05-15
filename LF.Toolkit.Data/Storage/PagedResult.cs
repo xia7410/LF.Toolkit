00468401c9b3e5e3602b092ec62d0aa9bb2fc023
bigsupersniper <linfeng.fjut@gmail.com>
2015-05-15 11:23:23 +0800

    添加分页查询结果类

---
+++ LF.Toolkit.Data/Storage/PagedResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LF.Toolkit.Data.Storage
+{
+    /// <summary>
+    /// 表示分页查询结果类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PagedResult<T> 
+    {
+        /// <summary>
+        /// 获取或设置分页结果集合
+        /// </summary>
+        IEnumerable<T> Results { get; set; }
+
+        /// <summary>
+        /// 获取或设置分页查询记录总数
+        /// </summary>
+        public long Total { get; set; }
+    }
+}
