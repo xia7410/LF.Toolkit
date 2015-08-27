66d2c885c3edc3e5d1aab551c030b0b8350f3671
bigsupersniper <linfeng.fjut@gmail.com>
2015-08-27 16:48:07 +0800

    添加类库生成批处理build.bat文件

---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo off
+
+set fdir=%WINDIR%\Microsoft.NET\Framework64
+
+if not exist %fdir% (
+	set fdir=%WINDIR%\Microsoft.NET\Framework
+)
+
+set msbuild=%fdir%\v4.0.30319\msbuild.exe
+
+%msbuild% LF.Toolkit.Data\LF.Toolkit.Data.csproj /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\build\LF.Toolkit.Data
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% LF.Toolkit.Data.NET45\LF.Toolkit.Data.NET45.csproj /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\build\LF.Toolkit.Data.NET45
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% LF.Toolkit.MongoDB.NET45\LF.Toolkit.MongoDB.NET45.csproj /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\build\LF.Toolkit.MongoDB.NET45
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% LF.Toolkit.Util\LF.Toolkit.Util.csproj /p:Configuration=Release /t:Clean;Rebuild /p:OutputPath=..\build\LF.Toolkit.Util
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+pause
