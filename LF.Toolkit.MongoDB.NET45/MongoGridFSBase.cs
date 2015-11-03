c9efabc53335f0df6a6c1f1676278d3f12d415f5
bigsupersniper <linfeng.fjut@gmail.com>
2015-11-03 13:43:03 +0800

    添加GridFS存储基类 更新MongoDB驱动到2.1.0 升级版主本号到2.1

---
+++ LF.Toolkit.MongoDB.NET45/MongoGridFSBase.cs
@@ -0,0 +1,158 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LF.Toolkit.MongoDB
+{
+    public abstract class MongoGridFSBase : MongoStorageBase
+    {
+        public GridFSBucketOptions BucketOptions { get; private set; }
+
+        public MongoGridFSBase(IMongoStorageConfig config, string databaseName, string bucketName, int chunkSizeBytes = 261120)
+            : this(config, databaseName, bucketName, new GridFSBucketOptions
+            {
+                BucketName = bucketName,
+                ChunkSizeBytes = chunkSizeBytes,
+                WriteConcern = WriteConcern.WMajority,
+                ReadPreference = ReadPreference.Secondary
+            })
+        {
+
+        }
+
+        public MongoGridFSBase(IMongoStorageConfig config, string databaseName, string bucketName, GridFSBucketOptions options)
+            : base(config, databaseName, bucketName)
+        {
+            BucketOptions = options;
+        }
+
+        /// <summary>
+        /// Uploads a file (or a new revision of a file) to GridFS.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="source"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public Task<ObjectId> UploadFromBytesAsync(string filename, byte[] source, GridFSUploadOptions options = null)
+        {
+            var bucket = new GridFSBucket(base.GetDatabase(), BucketOptions);
+            return bucket.UploadFromBytesAsync(filename, source, options);
+        }
+
+        /// <summary>
+        /// Uploads a file (or a new revision of a file) to GridFS.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="source"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public Task<ObjectId> UploadFromStreamAsync(string filename, Stream source, GridFSUploadOptions options = null)
+        {
+            var bucket = new GridFSBucket(base.GetDatabase(), BucketOptions);
+            return bucket.UploadFromStreamAsync(filename, source, options);
+        }
+
+        /// <summary>
+        /// Opens a Stream that can be used by the application to write data to a GridFS file.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public Task<GridFSUploadStream> OpenUploadStreamAsync(string filename, GridFSUploadOptions options = null)
+        {
+            var bucket = new GridFSBucket(base.GetDatabase(), BucketOptions);
+            return bucket.OpenUploadStreamAsync(filename, options);
+        }
+
+        /// <summary>
+        /// Downloads a file stored in GridFS and returns it as a byte array.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public Task<byte[]> DownloadAsBytesAsync(ObjectId id, GridFSDownloadOptions options = null)
+        {
+            var bucket = new GridFSBucket(base.GetDatabase(), BucketOptions);
+            return bucket.DownloadAsBytesAsync(id, options);
+        }
+
+        /// <summary>
+        /// Downloads a file stored in GridFS and returns it as a byte array.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public Task<byte[]> DownloadAsBytesByNameAsync(string filename, GridFSDownloadByNameOptions options = null)
+        {
+            var bucket = new GridFSBucket(base.GetDatabase(), BucketOptions);
+            return bucket.DownloadAsBytesByNameAsync(filename, options);
+        }
+
+        /// <summary>
+        /// Downloads a file stored in GridFS and writes the contents to a stream.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="destination"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public Task DownloadToStreamAsync(ObjectId id, Stream destination, GridFSDownloadOptions options = null)
+        {
+            var bucket = new GridFSBucket(base.GetDatabase(), BucketOptions);
+            return bucket.DownloadToStreamAsync(id, destination, options);
+        }
+
+        /// <summary>
+        /// Downloads a file stored in GridFS and writes the contents to a stream.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="destination"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public Task DownloadToStreamAsync(string filename, Stream destination, GridFSDownloadByNameOptions options = null)
+        {
+            var bucket = new GridFSBucket(base.GetDatabase(), BucketOptions);
+            return bucket.DownloadToStreamByNameAsync(filename, destination, options);
+        }
+
+        /// <summary>
+        /// Deletes a file from GridFS.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Task DeleteAsync(ObjectId id)
+        {
+            var bucket = new GridFSBucket(base.GetDatabase(), BucketOptions);
+            return bucket.DeleteAsync(id);
+        }
+
+        /// <summary>
+        /// Renames a GridFS file.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="newFilename"></param>
+        /// <returns></returns>
+        public Task RenameAsync(ObjectId id, string newFilename)
+        {
+            var bucket = new GridFSBucket(base.GetDatabase(), BucketOptions);
+            return bucket.RenameAsync(id, newFilename);
+        }
+
+        /// <summary>
+        ///  Finds matching entries from the files collection.
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public Task<IAsyncCursor<GridFSFileInfo>> FindAsync(FilterDefinition<GridFSFileInfo> filter, GridFSFindOptions options = null)
+        {
+            var bucket = new GridFSBucket(base.GetDatabase(), BucketOptions);
+            return bucket.FindAsync(filter, options);
+        }
+    }
+}
--- LF.Toolkit.UnitTests.NET45/MongoStorageBaseTests.cs
+++ LF.Toolkit.UnitTests.NET45/MongoStorageBaseTests.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using LF.Toolkit.DataEngine;
+using System.IO;
+using MongoDB.Driver.GridFS;
+using System.Text;
+using System.Linq;
+using MongoDB.Driver;
 
 namespace LF.Toolkit.UnitTests.NET45
 {
@@ -108,6 +113,43 @@ namespace LF.Toolkit.UnitTests.NET45
         }
     }
 
+    public interface IBigDataGridFS : IBootstrap
+    {
+        Task<ObjectId> Upload(FileStream fs);
+
+        Task<byte[]> Open(ObjectId id);
+
+        Task<GridFSFileInfo> FindOne(string md5);
+    }
+
+    public class BigDataGridFS : MongoGridFSBase, IBigDataGridFS
+    {
+        public BigDataGridFS(IMongoStorageConfig config)
+            : base(config, "im", "bigdata")
+        {
+
+        }
+
+        Task<ObjectId> IBigDataGridFS.Upload(FileStream fs)
+        {
+            return base.UploadFromStreamAsync(Path.GetFileName(fs.Name), fs);
+        }
+
+        Task<byte[]> IBigDataGridFS.Open(ObjectId id)
+        {
+            return base.DownloadAsBytesAsync(id);
+        }
+
+        async Task<GridFSFileInfo> IBigDataGridFS.FindOne(string filename)
+        {
+            var builder = Builders<GridFSFileInfo>.Filter.Eq<string>(i => i.Filename, filename);
+            var cursor = await base.FindAsync(builder, new GridFSFindOptions { Limit = 1 });
+            var list = await cursor.ToListAsync();
+
+            return list.FirstOrDefault();
+        }
+    }
+
     #endregion
 
     [TestClass]
@@ -125,6 +167,36 @@ namespace LF.Toolkit.UnitTests.NET45
             var entity = await storage.FindOneAsync("552b3d904a4a961d4c0d5001");
 
             Assert.IsNotNull(entity);
+        }
+
+        [TestMethod]
+        public async Task TestTransferGridFS()
+        {
+            var config = new MongoStorageConfig();
+            config.LoadFrom("mongocfg.json");
+            var bootstrap = StorageBootstrapProvider.CreateBootstrap<MongoStorageBase, MongoStroageBootstrap, IMongoStorageConfig>(config, GetType().Assembly);
+            var gridfs = bootstrap.CreateInstanceRef<IBigDataGridFS>();
+
+            Assert.IsInstanceOfType(gridfs, typeof(IBigDataGridFS));
+            //upload
+            var objectId = await gridfs.Upload(File.OpenRead("mongocfg.json"));
+            //download
+            var buffers = await gridfs.Open(objectId);
+            Assert.IsNotNull(buffers);
+        }
+
+        [TestMethod]
+        public async Task TestFindGridFS()
+        {
+            var config = new MongoStorageConfig();
+            config.LoadFrom("mongocfg.json");
+            var bootstrap = StorageBootstrapProvider.CreateBootstrap<MongoStorageBase, MongoStroageBootstrap, IMongoStorageConfig>(config, GetType().Assembly);
+            var gridfs = bootstrap.CreateInstanceRef<IBigDataGridFS>();
+
+            Assert.IsInstanceOfType(gridfs, typeof(IBigDataGridFS));
+            var gfs = await gridfs.FindOne("mongocfg.json");
+
+            Assert.IsNotNull(gridfs);
         }
     }
 }
