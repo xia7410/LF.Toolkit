929bf89dd8d1fe673a664bdf667771544f93804d
bigsupersniper <linfeng.fjut@gmail.com>
2016-10-20 15:18:45 +0800

    GroupTask扩展

--- LF.Toolkit.Common/GroupTask.cs
+++ LF.Toolkit.Common/GroupTask.cs
@@ -25,7 +25,7 @@ namespace LF.Toolkit.Common
                     var list = new List<Task>();
                     foreach (var item in collection.Skip(i * groupCount).Take(groupCount))
                     {
-                        list.Add(Task.Run(async () => await func(item)));
+                        list.Add(Task.Run(() => func(item)));
                     }
 
                     if (list.Count > 0)
@@ -62,6 +62,38 @@ namespace LF.Toolkit.Common
                     }
                 }
             }
+        }
+
+        public static async Task<IEnumerable<U>> WhenAllAsGroup<T, U>(Func<T, Task<U>> func, IEnumerable<T> collection, int groupCount)
+        {
+            var results = new List<U>();
+            int total = collection.Count();
+            if (total > 0)
+            {
+                //共分成几组
+                int groups = total / groupCount;
+                if (total % groupCount != 0)
+                {
+                    groups += 1;
+                }
+                //分组等待任务执行完毕
+                for (int i = 0; i < groups; i++)
+                {
+                    var list = new List<Task<U>>();
+                    foreach (var item in collection.Skip(i * groupCount).Take(groupCount))
+                    {
+                        list.Add(Task.Run(() => func(item)));
+                    }
+
+                    if (list.Count > 0)
+                    {
+                        var ls = await Task.WhenAll(list);
+                        results.AddRange(ls);
+                    }
+                }
+            }
+
+            return results;
         }
     }
 }
--- LF.Toolkit.UnitTests/GroupTaskUnitTest.cs
+++ LF.Toolkit.UnitTests/GroupTaskUnitTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,11 +22,34 @@ namespace LF.Toolkit.UnitTests
             }
         }
 
+        Task<string> Get(string url)
+        {
+            var client = new HttpClient();
+            return client.GetStringAsync(url);
+        }
+
         [TestMethod]
         public void TestWaitAsGroup()
         {
             //GroupTask.WaitAllAsGroup<int>((i) => Console.WriteLine(i), m_List, 10);
             GroupTask.WaitAllAsGroup<int>((i) => { Console.WriteLine(i); return Task.FromResult(0); }, m_List, 10);
+        }
+
+        [TestMethod]
+        public async Task TestWhenAllAsGroup()
+        {
+            var urls = new string[]
+            {
+                "https://www.baidu.com",
+                "http://cn.bing.com/",
+                "https://wx.qq.com/",
+                "https://translate.google.cn/"
+            };
+            var list = await GroupTask.WhenAllAsGroup<string, string>((i) => Get(i), urls, 2);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
