34f4105f1390e6c75360f5eee7f56f8826298a10
bigsupersniper <bigsupersniper@gmail.com>
2017-08-23 11:09:56 +0800

    添加基于autofac的代理存储基类 添加注入容器创建类

---
+++ LF.Toolkit.IOC/BaseProxyStorage.cs
@@ -0,0 +1,117 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LF.Toolkit.IOC
+{
+    /// <summary>
+    /// 代理存储接口标识
+    /// </summary>
+    public interface IProxyStorage
+    {
+        /// <summary>
+        /// 存储类容器
+        /// </summary>
+        IContainer Container { get; }
+    }
+
+    /// <summary>
+    /// 代理存储基类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class BaseProxyStorage<T> : IProxyStorage
+    {
+        /// <summary>
+        /// 获取当前代理的存储类实例
+        /// </summary>
+        public T ProxyStorage { get; private set; }
+
+        /// <summary>
+        /// 获取存储类容器
+        /// </summary>
+        public IContainer Container { get; private set; }
+
+        public BaseProxyStorage(IContainer container)
+        {
+            Container = container;
+            ProxyStorage = container.Resolve<T>();
+        }
+    }
+
+    /// <summary>
+    /// 带有缓存的代理存储基类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class BaseMemoryCacheProxyStorage<T> : BaseProxyStorage<T>
+    {
+        readonly MemoryCache m_Cached;
+
+        public BaseMemoryCacheProxyStorage(IContainer container)
+            : base(container)
+        {
+            m_Cached = new MemoryCache(this.GetType().FullName + "." + Guid.NewGuid().ToString("N"));
+        }
+
+        /// <summary>
+        /// 使用键和值将某个缓存项插入缓存中，并指定基于时间的过期详细信息
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="expiration"></param>
+        protected void SetCache(string key, object value, TimeSpan? expiration = null)
+        {
+            if (!string.IsNullOrEmpty(key) && value != null)
+            {
+                var offset = DateTimeOffset.MinValue;
+                if (expiration.HasValue && expiration.Value > TimeSpan.Zero)
+                {
+                    offset = DateTimeOffset.Now.Add(expiration.Value);
+                }
+                else
+                {
+                    offset = DateTimeOffset.MaxValue;
+                }
+
+                m_Cached.Set(key, value, offset);
+            }
+        }
+
+        /// <summary>
+        /// 获取指定名称的缓存对象
+        /// </summary>
+        /// <typeparam name="K"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        protected bool TryGetCache<K>(string key, out K value)
+        {
+            var item = m_Cached.Get(key);
+            if (item != null && typeof(K).IsAssignableFrom(item.GetType()))
+            {
+                value = (K)item;
+                return true;
+            }
+            else
+            {
+                value = default(K);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 删除指定名称的缓存对象
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        protected bool RemoveCache(string key)
+        {
+            var obj = m_Cached.Remove(key);
+
+            return obj != null;
+        }
+    }
+}
---
+++ LF.Toolkit.IOC/InjectionContainerBuilder.cs
@@ -0,0 +1,112 @@
+﻿using Autofac;
+using Autofac.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LF.Toolkit.IOC
+{
+    /// <summary>
+    /// 注入容器生成类
+    /// </summary>
+    public static class InjectionContainerBuilder
+    {
+        /// <summary>
+        /// Register components to be created through reflection. which components is marked InjectableAttribute
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <param name="func"></param>
+        /// <param name="beforBuild"></param>
+        /// <param name="afterBuild"></param>
+        /// <returns></returns>
+        public static IContainer Build(Assembly assembly, Func<Type, IDictionary<Type, object>> func = null, Action<ContainerBuilder> beforBuild = null)
+        {
+            var containerBuilder = new ContainerBuilder();
+            var types = assembly.GetTypes().Where(i => i.IsClass && !i.IsAbstract);
+            foreach (var t in types)
+            {
+                var attr = t.GetCustomAttribute<InjectableAttribute>(true);
+                if (attr == null)
+                {
+                    continue;
+                }
+                Register(containerBuilder, t, parameters: func != null ? func.Invoke(t) : null, asSelf: attr.AsSelf, asImplementedInterfaces: attr.AsImplementedInterfaces, singleInstance: attr.SingleInstance);
+            }
+
+            if (beforBuild != null)
+            {
+                beforBuild.Invoke(containerBuilder);
+            }
+
+            return containerBuilder.Build();
+        }
+
+        /// <summary>
+        /// Register components to be created through reflection. which components Inherited from T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="assembly"></param>
+        /// <param name="func"></param>
+        /// <param name="asSelf"></param>
+        /// <param name="asImplementedInterfaces"></param>
+        /// <param name="singleInstance"></param>
+        /// <param name="beforBuild"></param>
+        /// <param name="afterBuild"></param>
+        /// <returns></returns>
+        public static IContainer Build<T>(Assembly assembly, Func<Type, IDictionary<Type, object>> func = null, bool asSelf = true, bool asImplementedInterfaces = true, bool singleInstance = true
+            , Action<ContainerBuilder> beforBuild = null)
+        {
+            var containerBuilder = new ContainerBuilder();
+            var types = assembly.GetTypes().Where(i => i.IsClass && !i.IsAbstract && typeof(T).IsAssignableFrom(i));
+            foreach (var t in types)
+            {
+                Register(containerBuilder, t, parameters: func != null ? func.Invoke(t) : null);
+            }
+
+            if (beforBuild != null)
+            {
+                beforBuild.Invoke(containerBuilder);
+            }
+
+            return containerBuilder.Build();
+        }
+
+        /// <summary>
+        /// Register a component to be created through reflection.
+        /// </summary>
+        /// <param name="containerBuilder"></param>
+        /// <param name="type"></param>
+        /// <param name="parameters"></param>
+        /// <param name="asSelf"></param>
+        /// <param name="asImplementedInterfaces"></param>
+        /// <param name="singleInstance"></param>
+        static void Register(ContainerBuilder containerBuilder, Type type, IDictionary<Type, object> parameters = null, bool asSelf = true, bool asImplementedInterfaces = true, bool singleInstance = true)
+        {
+            IEnumerable<Parameter> list = null;
+            if (parameters != null)
+            {
+                list = parameters.Select(p => new TypedParameter(p.Key, p.Value));
+            }
+            var builder = containerBuilder.RegisterType(type);
+            if (list != null)
+            {
+                builder = builder.WithParameters(list);
+            }
+            if (asSelf)
+            {
+                builder = builder.AsSelf();
+            }
+            if (asImplementedInterfaces)
+            {
+                builder = builder.AsImplementedInterfaces();
+            }
+            if (singleInstance)
+            {
+                builder = builder.SingleInstance();
+            }
+        }
+    }
+}
